482b298cc3a622baa4b60db4c95e866442197c34
basicasian <e11921607@student.tuwien.ac.at>
2022-06-07 21:58:49 +0200

    adapted position of light boxes

--- ECG_Solution/src/Main.cpp
+++ ECG_Solution/src/Main.cpp
@@ -196,9 +196,6 @@ int main(int argc, char** argv)
 		// Create textures
 		std::shared_ptr<ShadowMapTexture> shadowMapTexture = std::make_shared<ShadowMapTexture>(window_width, window_height);
 
-
-		//std::shared_ptr<VideoTexture> videoTexture = std::make_shared<VideoTexture>("assets/textures/videoframes/frame_", ".png");
-		//std::shared_ptr<Texture> videoTexture = std::make_shared<Texture>("assets/textures/videotextures/minions/frame0.jpg", shadowMapTexture->getHandle(), "video");
 		std::shared_ptr<Texture> videoTexture = std::make_shared<Texture>("assets/textures/videotextures/goodgame/frame_0.jpg", shadowMapTexture->getHandle(), "video");
 		std::shared_ptr<Texture> imageTexture = std::make_shared<Texture>("assets/textures/platform.jpg", shadowMapTexture->getHandle(), "image");
 		std::shared_ptr<Texture> woodTexture = std::make_shared<Texture>("assets/textures/wood_texture.dds", shadowMapTexture->getHandle(), "dds");
@@ -215,9 +212,11 @@ int main(int argc, char** argv)
 		std::shared_ptr<Material> lightMaterial = std::make_shared<TextureMaterial>(lightShader);
 
 		// Create geometry
-		Geometry mainBox(glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 0.0f)), Geometry::createCubeGeometry(0.5f, 0.5f, 0.5f), woodTextureMaterial);
+		Geometry mainBox(glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, 5.0f)), Geometry::createCubeGeometry(1.5f, 0.5f, 1.5f), imageTextureMaterial);
 		Geometry mainBox2(glm::translate(glm::mat4(1.0f), glm::vec3(5.0f, 0.0f, 0.0f)), Geometry::createCubeGeometry(1.5f, 0.5f, 1.5f), imageTextureMaterial);
-		Geometry mainBox3(glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 2.0f, -3.0f)), Geometry::createCubeGeometry(2.0f, 1.5f, 0.01f), videoTextureMaterial);
+		Geometry mainBox3(glm::translate(glm::mat4(1.0f), glm::vec3(-5.0f, -2.0f, -3.0f)), Geometry::createCubeGeometry(1.5f, 0.5f, 1.5f), imageTextureMaterial);
+
+		Geometry videoScreen(glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 2.0f, -3.0f)), Geometry::createCubeGeometry(2.0f, 1.5f, 0.01f), videoTextureMaterial);
 
 		glm::mat4 catModel = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 10.0f, 0.0f));
 		ModelLoader cat("assets/objects/cat/cat.obj", catModel, catModelMaterial);
@@ -266,17 +265,23 @@ int main(int argc, char** argv)
 
 		#pragma region point lights
 		// turqoise
-		PointLight pointL1(glm::vec3(0.0f, 3.0f, 3.0f), glm::vec3(1.0f, 1.0f, -2.0f), glm::vec3(1.0f, 0.7f, 1.8f));
+		PointLight pointL1(glm::vec3(0.0f, 3.0f, 3.0f), glm::vec3(7.0f, 0.0f, -10.0f), glm::vec3(1.0f, 0.7f, 1.8f));
 		pointLights.push_back(pointL1);
 		// yellow
-		PointLight pointL2(glm::vec3(5.0f, 5.0f, 0.0f), glm::vec3(1.0f, 1.2f, 1.0f), glm::vec3(1.0f, 0.7f, 1.8f));
+		PointLight pointL2(glm::vec3(3.0f, 3.0f, 0.0f), glm::vec3(15.0f, 4.0f, 1.0f), glm::vec3(1.0f, 0.7f, 1.8f));
 		pointLights.push_back(pointL2);
 		// green
-		PointLight pointL3(glm::vec3(0.0f, 5.0f, 0.0f), glm::vec3(-2.0f, 1.0f, 1.0f), glm::vec3(1.0f, 0.7f, 1.8f));
+		PointLight pointL3(glm::vec3(0.0f, 3.0f, 0.0f), glm::vec3(-30.0f, 14.0f, 35.0f), glm::vec3(1.0f, 0.7f, 1.8f));
 		pointLights.push_back(pointL3);
 		// white 
-		PointLight pointL4(glm::vec3(3.0f, 3.0f, 3.0f), glm::vec3(-2.0f, 1.0f, -2.0f), glm::vec3(1.0f, 0.7f, 1.8f));
+		PointLight pointL4(glm::vec3(3.0f, 3.0f, 3.0f), glm::vec3(-25.0f, 5.0f, -25.0f), glm::vec3(1.0f, 0.7f, 1.8f));
 		pointLights.push_back(pointL4);
+		// pink
+		PointLight pointL5(glm::vec3(5.0f, 0.0f, 5.0f), glm::vec3(20.0f, 5.0f, -35.0f), glm::vec3(1.0f, 0.7f, 1.8f));
+		pointLights.push_back(pointL5);
+		// white
+		PointLight pointL6(glm::vec3(3.0f, 3.0f, 3.0f), glm::vec3(20.0f, 3.0f, 15.0f), glm::vec3(1.0f, 0.7f, 1.8f));
+		pointLights.push_back(pointL6);
 
 		#pragma endregion
 
@@ -326,6 +331,8 @@ int main(int argc, char** argv)
 			mainBox.drawShader(depthShader.get());
 			mainBox2.drawShader(depthShader.get());
 			mainBox3.drawShader(depthShader.get());
+
+			videoScreen.drawShader(depthShader.get());
 			cat.SetModelMatrix(glm::translate(glm::mat4(1.0f), btCat.getPosition()));
 			cat.DrawShader(depthShader.get());
 			scene.DrawShader(depthShader.get());
@@ -341,8 +348,9 @@ int main(int argc, char** argv)
 			// render
 			mainBox.draw();
 			mainBox2.draw();
-			
 			mainBox3.draw();
+			
+			videoScreen.draw();
 			cat.SetModelMatrix(glm::translate(glm::mat4(1.0f), btCat.getPosition()));
 			cat.Draw();
 			scene.Draw();
@@ -350,7 +358,11 @@ int main(int argc, char** argv)
 			// light cubes
 			for (int i = 0; i < pointLights.size(); i++) {
 				PointLight& pointL = pointLights[i];
-				Geometry lightbox(glm::translate(glm::mat4(1.0f), pointL._position), Geometry::createCubeGeometry(0.5f, 0.5f, 0.5f), lightMaterial);
+
+				glm::mat4 trans = glm::mat4(1.0f);
+				trans = glm::translate(trans, pointL._position);
+				trans = glm::rotate(trans, glm::radians(15.0f * i), glm::vec3(1.0, 1.0, 1.0));
+				Geometry lightbox(trans , Geometry::createCubeGeometry(2.0f * i, 2.0f * i, 2.0f * i), lightMaterial);
 
 				setPerFrameUniformsLight(lightShader.get(), pointL, lightMaterial);
 				lightbox.drawShader(lightShader.get());
